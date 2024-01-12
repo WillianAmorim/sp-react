6e3bec5b76a0697d48f2651d015ecdb9a431efd4
WillianAmorim <willian.amorim1@outlook.com>
2024-01-12 11:24:25 -0500

    finalizado pagina drogasil

--- src/pages/Drogasil.jsx
+++ src/pages/Drogasil.jsx
@@ -5,16 +5,18 @@ import Termos from '../components/Termos'
 import Contato from '../components/Contato'
 import Localizacao from '../components/Localizacao'
 import Sobre from '../components/Sobre'
+import Img from '../../public/IMAGENS_SITE_SAO_PAULO/AREAS DE ATUAÇÃO/BTS (BUILT TO SUIT)/Drogasil - Cohatrac I.jpg'
 
 const Drogasil = () => {
     return (
-        <>
+        <>  
             <Header />
+            <img src={Img} alt="" width="100%" height="700px"/>
             <Sobre />
             <Localizacao />
             <Contato />
-            {/* <Footer />
-            <Termos /> */}
+            <Footer />
+            <Termos />
         </>
     )
 }
