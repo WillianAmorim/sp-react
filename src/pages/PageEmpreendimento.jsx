9c89970fa9fbd08006b30a4c149b006ff6961d90
WillianAmorim <willian.amorim1@outlook.com>
2024-03-04 16:21:20 -0500

    teste de promisse all

--- src/pages/PageEmpreendimento.jsx
+++ src/pages/PageEmpreendimento.jsx
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { useTimeout } from 'react-use'; // Importe o hook useTimeout
-
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Termos from '../components/Termos'
-import FormEmpreendimento from '../components/FormEmpreendimento'
-import Localizacao from '../components/Localizacao'
-import Sobre from '../components/Sobre'
-import PlantaBaixa from '../components/Planta'
-import CarroselEmpreendimento from '../components/CarroselEmpreendimento'
-import Navbar from '../components/Navbar'
-import data from '../../public/Json/JsonEmpreendimentos.json'
-import Whatsapp from '../components/Whatsapp'
-import Cadastro from '../components/Cadastro'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Termos from '../components/Termos';
+import FormEmpreendimento from '../components/FormEmpreendimento';
+import Localizacao from '../components/Localizacao';
+import Sobre from '../components/Sobre';
+import PlantaBaixa from '../components/Planta';
+import CarroselEmpreendimento from '../components/CarroselEmpreendimento';
+import Navbar from '../components/Navbar';
+import data from '../../public/Json/JsonEmpreendimentos.json';
+import Whatsapp from '../components/Whatsapp';
+import Cadastro from '../components/Cadastro';
 
 import { useParams } from 'react-router-dom';
 
 const PageEmpreendimento = () => {
-
     const { pagEmpreendimento } = useParams();
+    const [filterCard, setFilterCard] = useState(null);
 
-    const [filterCard] = data.filter((empreendimento) => empreendimento.name.toLowerCase().replace(/ /g, "-").normalize("NFD").replace(/[\u0300-\u036f]/g, "") === pagEmpreendimento)
-    console.log(filterCard)   
+    useEffect(() => {
+        // Função para pré-carregar as imagens do empreendimento
+        const preloadImages = async () => {
+            const empreendimento = data.find(empreendimento => empreendimento.name.toLowerCase().replace(/ /g, "-").normalize("NFD").replace(/[\u0300-\u036f]/g, "") === pagEmpreendimento);
 
-    const [pageLoaded, setPageLoaded] = useState(false);
+            if (empreendimento) {
+                const images = empreendimento.images.map(image => new Promise((resolve, reject) => {
+                    const img = new Image();
+                    img.src = image;
+                    img.onload = resolve;
+                    img.onerror = reject;
+                }));
 
-    useTimeout(() => {
-        setPageLoaded(true);
-    }, 15000); // Tempo de simulação: 15 segundos
+                try {
+                    await Promise.all(images);
+                    setFilterCard(empreendimento);
+                } catch (error) {
+                    console.error('Erro ao pré-carregar imagens:', error);
+                }
+            }
+        };
+
+        preloadImages();
+    }, [pagEmpreendimento]);
+
+    if (!filterCard) {
+        // Se as imagens ainda estiverem sendo carregadas, pode-se exibir uma mensagem de carregamento ou um indicador de progresso
+        return <div>Loading...</div>;
+    }
 
     return (
         <>  
