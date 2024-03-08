73bfc013185e62ef187736e050f7e2925adfdf88
WillianAmorim <willian.amorim1@outlook.com>
2024-03-07 23:57:38 -0500

    adicionado componente de loader

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -4,8 +4,7 @@ import { ClipLoader } from 'react-spinners';
 const LoadingAnimation = () => {
   return (
     <div id='loader' className="loader-container">
-      <ClipLoader color="#36D7B7" loading={true} size={35} />
-      <p>Carregando...</p>
+      <ClipLoader color="#121D40" loading={true} size={35} />
     </div>
   );
 };
