2e969d49c862a674f0035a4cb7f53219ffafdffb
WillianAmorim <willian.amorim1@outlook.com>
2024-03-08 00:17:39 -0500

    finalizado componente loader

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -4,10 +4,12 @@ import { LoaderContainer } from '../styles/Loader';
 
 const LoadingAnimation = () => {
   return (
-    <LoaderContainer className="loader-container">
-      <ClipLoader id='loader' color="#121D40" loading={true} size={55} />
-      <p>Carregando...</p>
-    </LoaderContainer>
+    <div className="loader-container">
+      <LoaderContainer id='loader'>
+        <ClipLoader color="#121D40" loading={true} size={55} />
+        <p>Carregando...</p>
+      </LoaderContainer>
+    </div>
   );
 };
 
--- src/styles/Loader.js
+++ src/styles/Loader.js
@@ -1,18 +1,13 @@
 import styled from "styled-components";
 
 export const LoaderContainer = styled.div`
+    width: 100%;
+    height: 100vh;
 
-    #loader {
-        position: absolute;
-        right: 50%;
-        top: 40%;
-    }
-
-    p {
-        position: absolute;
-        right: 48%;
-        top: 50%;
-    }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     @media only screen and (min-width: 767px) {
 
