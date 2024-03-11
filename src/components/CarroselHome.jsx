b530dd4a8bab793f0e6951145c95b2ef61a0174d
WillianAmorim <willian.amorim1@outlook.com>
2024-03-11 13:24:47 -0500

    alterado formato de imagens do banner da home

--- src/components/CarroselHome.jsx
+++ src/components/CarroselHome.jsx
@@ -4,10 +4,10 @@ import { EffectFade, Navigation, Pagination, Autoplay } from 'swiper/modules'
 import { Link } from 'react-router-dom'
 
 
-import EasyResidence from '../assets/Easy.jpg'
-import GranImperial from '../assets/Gran.jpg'
-import IOA from '../assets/IOA.jpg'
-import Medical from '../assets/MedicalCenter.jpg'
+import EasyResidence from '../assets/Easy.webp'
+import GranImperial from '../assets/Gran.webp'
+import IOA from '../assets/IOA.webp'
+import Medical from '../assets/MedicalCenter.webp'
 
 import { CarroselContainer } from '../styles/Carrosel-Primeiro';
 import { BsBuilding } from "react-icons/bs";
@@ -39,7 +39,7 @@ const CarroselHome = () => {
               <li className="li-swiper-container1"><IoCarSharp />1 ou 2 vagas</li>
               <li className="li-swiper-container1"><GiShower />1 ou duas suítes</li>
             </ul>
-            <Link to={'/empreendimento/bts/ioa'}>Conheça o imóvel</Link>
+            <Link to={'/empreendimento/bts/medical-center'}>Conheça o imóvel</Link>
           </div>
           <img className='slide-item' src={Medical} alt="" />
 
