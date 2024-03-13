ab0513aced2f4a032894b20022017993d4ca6fdd
WillianAmorim <willian.amorim1@outlook.com>
2024-03-13 15:05:02 -0500

    prjeto praticamente finalizado

--- src/components/Locacoes.jsx
+++ src/components/Locacoes.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { LocacaoContainer } from '../styles/Locacoes'
 
 import ImgComercial from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/COMERCIAL/Costa Atacadao/AtacadaoPrincipal.webp'
-import ImgCorporativo from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/CORPORATIVO/Defensoria Publica da Uniao (DPU)/Principal.jpg'
-import ImgGalpao from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/GALPAO-LOGISTICO/JB Transportes/JBTransportesPrincipal.jpg'
-import ImgResidencial from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/RESIDENCIAL/Edificio Ponta Negra/Principal.jpg'
+import ImgCorporativo from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/CORPORATIVO/Defensoria Publica da Uniao (DPU)/Principal.webp'
+import ImgGalpao from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/GALPAO-LOGISTICO/JB Transportes/JBTransportesPrincipal.webp'
+import ImgResidencial from '/FOTOS-NOVO-GUI/LOCACAO DE IMOVEIS/RESIDENCIAL/Edificio Cidade do Porto/Principal.webp'
 
 
 import { Link } from 'react-router-dom'
